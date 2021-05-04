56d38ed0fdb3ea6f894934cd334bd4461431b85f
BourbonDefi <80276717+bourbondefi@users.noreply.github.com>
2021-05-04 16:00:12 -0400

    price

--- src/constants/goldPriceContracts.ts
+++ src/constants/goldPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0x7595bb34bf391c3de0a6878a36d06223b2bfd404',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x6392aC63A851497Fc1F86946923a365162518E30'
+  lpAddress: '0x9bc90e1e33a8a85dfa1db813af027fd342d2af0b'
 }
 
 export default priceContracts
--- src/constants/tsbPriceContracts.ts
+++ src/constants/tsbPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
   cakeAddress: '0xA37F71f092F891757d23Dd350f57C78E8469c026',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0xe336b16B7d26B94F3E86Fe124Be5207162C22d90'
+  lpAddress: '0x2A9272FfDE448ba1F3538Ad85811C42217A7f46f'
 }
 
 export default priceContracts
